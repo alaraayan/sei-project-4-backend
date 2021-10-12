1fd7cfdb3d37c9e95a5e2bca3320cc4240e63e48
alara <alaraayan@hotmail.com>
2021-10-12 22:34:17 +0100

    redeploying

--- jwt_auth/serializers.py
+++ jwt_auth/serializers.py
@@ -14,20 +14,20 @@ class UserSerializer(serializers.ModelSerializer):
     # password_confirmation field, too.
     password = serializers.CharField(write_only=True)
     password_confirmation = serializers.CharField(write_only=True)
-    print("🍓made this far")
+    
     # We do this to add our own custom validation. Must be called validate for the serializer
     # to know what to use it for. The second argument, data, is the incoming request object
     def validate(self, data):
         # You can use `.pop` on a dictionary in Python, which will remove the property and
         # store it in the variable you assign it to. Do this for password & password_confirmation so
         # we can compare them to make sure they're the same and, if so, validate the password.
-        password = data.pop("password")
-        password_confirmation = data.pop("password_confirmation")
-        print("🍉made this far")
+        password = data.pop('password')
+        password_confirmation = data.pop('password_confirmation')
+        
         # First compare the password and password_confirmation. If not, raise a ValidationError
         # and send back an error message.
         if password != password_confirmation:
-            raise serializers.ValidationError({"password_confirmation": "Passwords do not match"})
+            raise serializers.ValidationError({'password_confirmation': 'Passwords do not match'})
         
         #! This is where we can use the password validator from Django. We use a try/except.
         #? `if not settings.DEBUG` means we don't validate in development but we do in production
@@ -35,15 +35,15 @@ class UserSerializer(serializers.ModelSerializer):
             try:
                 # Run in through the built-in Django validator
                 validation.validate_password(password=password)
-                print("🍇made this far")
+            
             # Rename the ValidationError for clarity as we'll use the ValidationError from Django
             # But we'll also raise a serializers.ValidationError, which would get confusing!
             except ValidationError as err:
-                raise serializers.ValidationError({"password": err.messages})
+                raise serializers.ValidationError({'password': err.messages})
         
         # Attach the hashed password to the data dictionary.
-        data["password"] = make_password(password)
-        print("🍑made this far")
+        data['password'] = make_password(password)
+        
         # So, we removed the original password and password_confirmation.
         # Compared and validated the password.
         # Then once that was all OK, hashed the password and added it back to the dictionary
@@ -52,4 +52,4 @@ class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = User
-        fields = "__all__"
+        fields = '__all__'
--- jwt_auth/views.py
+++ jwt_auth/views.py
@@ -1,11 +1,12 @@
-from datetime import datetime, date, timedelta
+from datetime import datetime, timedelta
 from rest_framework.exceptions import NotFound
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
 
-
+from datetime import datetime, date, timedelta
 from django.contrib.auth import get_user_model
 from django.conf import settings
 
@@ -13,22 +14,20 @@ import jwt
 
 from .serializers import UserSerializer
 from .populated import PopulatedUserSerializer
-
+from sprints.serializers import SprintSerializer
 
 User = get_user_model()
 
 
 class RegisterView(APIView):
     def post(self, request):
+        print(request.data)
         # Run the body of the request through the serializer for serialization and validation
         user_to_create = UserSerializer(data=request.data)
-        print(user_to_create.is_valid())
-        print(request.data)
+
         # Proceed if valid
         if user_to_create.is_valid():
-            print("🍒made this far")
             user_to_create.save()
-            print("🍍made this far")
             return Response({"message": "Registration Successful"}, status=status.HTTP_201_CREATED)
 
         # Send back 422 if not valid
